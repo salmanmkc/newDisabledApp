0f5d3a54d278dd65c7624c79a06e7cdc8f44edd6
Salman Muin Kayser Chishti <13schishti@Gmail.com>
2019-07-29 17:00:00 +0100

    using ComboBox instead of DropDownMenu, since I want to show the user what they have chosen, needs a method

--- App17/Views/MainPage.xaml.cs
+++ App17/Views/MainPage.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 using App17.ViewModels;
-
+using Windows.UI;
 using Windows.UI.Xaml.Controls;
 
 namespace App17.Views
@@ -13,6 +13,29 @@ namespace App17.Views
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void ColorComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Add "using Windows.UI;" for Color and Colors.
+            string colorName = e.AddedItems[0].ToString();
+            Color color;
+            switch (colorName)
+            {
+                case "Yellow":
+                    color = Colors.Yellow;
+                    break;
+                case "Green":
+                    color = Colors.Green;
+                    break;
+                case "Blue":
+                    color = Colors.Blue;
+                    break;
+                case "Red":
+                    color = Colors.Red;
+                    break;
+            }
+            //colorRectangle.Fill = new SolidColorBrush(color);
         }
     }
 }
